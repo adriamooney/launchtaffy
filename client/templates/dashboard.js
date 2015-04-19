3f01b40be3ad686cd0612a97bcee474e516ca992
TBD Labs <tbdlabs@TBDs-MacBook-Air.local>
2015-04-19 12:13:14 -0700

    removed company regex for now

--- client/templates/dashboard.js
+++ client/templates/dashboard.js
@@ -7,9 +7,9 @@ Template.dashboard.events({
 
         var val = template.find('#companyNameVal').value;
 
-        var reg = val.match(/^[a-z0-9]+(?:-[a-z0-9]+)*$/);
-        console.log(reg);
-        if(reg !== null) {
+        //var reg = val.match(/^[a-z0-9]+(?:-[a-z0-9]+)*$/);
+        //console.log(reg);
+        //if(reg !== null) {
             Meteor.call("getLinkedCompanyProfile", val, function(err, result) {
                 if(err) {
                     AppMessages.throw(err.reason, 'danger');
@@ -17,10 +17,10 @@ Template.dashboard.events({
                 }
             
             }); 
-        }
+       /* }
         else {
             AppMessages.throw('Oops, it appears you entered your company name incorrectly.', 'danger');
-        }
+        } */
 
         
     }
