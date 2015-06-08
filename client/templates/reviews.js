76886ec8ecdf0418037838ae77c6b3ed0c9c7cb7
TBD Labs <tbdlabs@TBDs-MacBook-Air.local>
2015-06-08 13:05:27 -0700

    reviews in progress

--- client/templates/reviews.js
+++ client/templates/reviews.js
@@ -28,4 +28,25 @@ Template.companyReviewForm.helpers({
 	}
 });
 
+Template.reviewItem.helpers({
+	reviewerName: function() {
+		console.log(this);
+		var reviewerId = this.commenterId;
+		console.log(reviewerId);
+		var reviewer = Meteor.users.findOne({_id: reviewerId});
+		console.log(reviewer.profile.firstName);
+		if(reviewer.profile.userType == 'company') {
+			var companyId = reviewer._id;
+			var company = Company.findOne({_id: companyId});
+			return company.name;
+		}
+		else {
+			return reviewer.profile.firstName +' '+ reviewer.profile.lastName;
+		}
+	},
+	reviewerImg: function() {
+		console.log(this);
+	}
+});
+
 
